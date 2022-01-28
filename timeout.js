6fbc48734efab8a8d1b656269b7faf4924a6c9cd
arifahan <95140381+arifahan@users.noreply.github.com>
2022-01-28 11:52:01 +0600

    ok

---
+++ timeout.js
@@ -0,0 +1,32 @@
+function doSomething(){
+    console.log('Hello');
+}
+
+console.log(1222);
+setTimeout(doSomething, 4000);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+setTimeout(function(){
+    console.log("Good Morning");
+}, 5000)
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+setTimeout(() => {
+    console.log("Good Night");
+}, 2000)
+console.log(4444);
+console.log(11111);
+console.log(11111);
+console.log(11111);
+setInterval(() => {
+    console.log("After 1000 ML Second")
+}, 2000);
+console.log(11111);
+console.log(11111);
+console.log(11111);
+console.log(11111);
